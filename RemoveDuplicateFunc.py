5bf10a10ec417f5f4763722ab1b4e778cc1391d2
Gabriel Moura Vargas <gab.mv@hotmail.com>
2018-10-10 18:57:55 -0300

    2 new basic functions

---
+++ RemoveDuplicateFunc.py
@@ -0,0 +1,12 @@
+# This program is used to remove duplicated values from a list passed by parameter
+l = [11,10,8,9,9,9,9,8,8,11]
+
+def compare(list):
+    newList = []
+    for x in list:
+        if x not in newList:
+            newList.append(x)    
+    return newList
+
+x = compare(l)
+print(x)
