1836b3bbdf5fd934ff956a840dcc446bdd9012c0
devanshiv123 <devanshiv123@gmail.com>
2018-10-11 22:14:56 +0530

    added comments

--- avg_of_numbers.py
+++ avg_of_numbers.py
@@ -4,6 +4,6 @@ n=int(input("Enter the number of elements to be inserted: "))
 a=[]
 for i in range(0,n):
     element=int(input("Enter element: "))
-    a.append(element)
+    a.append(element) #append() method takes a single item and adds it to end of list
 avg=sum(a)/n
 print("Average of elements in the list : ",round(avg,2))
--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,2 @@
+#This is a simple program to print Hello World! in python 
 print("Hello World!")
