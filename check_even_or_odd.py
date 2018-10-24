ab80d949fdf711425785dc4ec7a9d2f429e0c49f
kik-kik <kik@medicuja.com>
2018-10-24 19:19:52 +0200

    fixed a bug num did not exist outside validateEvenOrOddInput function

--- check_even_or_odd.py
+++ check_even_or_odd.py
@@ -1,19 +1,21 @@
 #__author__ = "Sanjay"
 
 def validateEvenOrOddInput(num):
-	'''
-	@param : num (int)
-	@desc: returns given input is odd or even
-	'''
-	if (type(num) == 'int'):			
-		if(num % 2==0):
-			print('{} is an even number'.format(num))
-		else:
-			print('{} is an odd number'.format(num))	
+    '''
+    @param : num (int)
+    @desc: returns given input is odd or even
+    '''
+
+    if isinstance(num, int):
+        if(num % 2==0):
+            print('{} is an even number'.format(num))
+        else:
+            print('{} is an odd number'.format(num))
 
 if __name__ == '__main__':
-	userInput = int(input('Input your number: '))
-	if (type(num) == 'int'):
-		validateEvenOrOddInput(userInput)
-	else:
-		print ("Given input is not valid.")
+    userInput = int(input('Input your number: '))
+
+    if isinstance(userInput, int):
+        validateEvenOrOddInput(userInput)
+    else:
+        print ("Given input is not valid.")
