6312c707913d6555f1d023475f72c53b5bef7ac2
Daniel Melo <38673841+danielx285@users.noreply.github.com>
2018-10-24 22:36:52 -0300

    Sum all number of 1 to n
    O algorithm permite calcular a soma dos numeros de 1 até n sem usar laços
    A small change since here it is preferable that we use python 3

--- sum_1_to_n.py
+++ sum_1_to_n.py
@@ -1,7 +1,8 @@
 #ALGORITHMS IS LIFE
-first_number, second_number = map(int, raw_input().split())
+#pls, please give two numbers a, b (a <= b)
+first_number, second_number = map(int, input().split())
 
 #used (a *(a +1)/2) - (b *(b + 1)/2)
 sum_total = (second_number*(second_number + 1))/2 - ((first_number-1)*(first_number ))/2
 
-print sum_total
+print (int(sum_total))
