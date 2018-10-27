afe77b31dbbccb7a3f1680f32e44f3a823e8fc6a
nishntr <nrai1185@gmail.com>
2018-10-27 22:22:41 +0530

    Added comments

--- print_even_numbers.py
+++ print_even_numbers.py
@@ -1,9 +1,11 @@
-n = int(input('Enter the number of terms:'))
+#A program to print n number of even numbers, taking input to n
 
-total_terms = 0
+n = int(input('Enter the number of terms:')) #taking input
+
+total_terms = 0                     #to check the no. of even numbers printed
 number = 0
-while total_terms <= n:
-    if number % 2 == 0:
+while total_terms <= n:             #checking if no. of printed numbers is still less than n
+    if number % 2 == 0:             #checking if the number is an even number
         print(number)
         total_terms += 1
     number += 1
