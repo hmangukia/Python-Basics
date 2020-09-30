2f798d437078351238348d91f677d7764013e9ba
Shrestha Baburam <54539725+Shrestha-Baburam@users.noreply.github.com>
2020-10-01 01:58:20 +0545

    Update ArithmeticOperators.py

--- ArithmeticOperators.py
+++ ArithmeticOperators.py
@@ -11,13 +11,20 @@ __author__ = 'Sanjay'
 # Subtraction (-)
 # Multiplication (*)
 # Division
-#
+# Integer Division
+# Modulo Division
+# Exponent
+
 # Task
 # Read two integers from STDIN and print three lines where:
 #
 # The first line contains the sum of the two numbers.
 # The second line contains the difference of the two numbers (first - second).
 # The third line contains the product of the two numbers.
+# The fourt line contains the division of the two numbers.
+# The fifth line contains the integer divisio of the two numbers.
+# The sixth line contains the modulo sivision of the two numbers.
+# The seventh line contains the first to the power of second numbers.
 # Input Format
 # The first line contains the first integer, numberA. The second line contains the second integer, numberB.
 #
@@ -35,11 +42,17 @@ __author__ = 'Sanjay'
 # 1
 # 6
 # 1.5
+# 1
+# 1
+# 9
 # Explanation
 # 3+2
 # 3-2
 # 3*2
 # 3/2
+# 3//2
+# 3%2
+# 3^2 or 3**2
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 numberA = int(raw_input())
@@ -49,3 +62,6 @@ print (numberA+numberB)
 print (numberA-numberB)
 print (numberA*numberB)
 print (numberA/numberB)
+print (numberA//numberB)
+print (numberA%numberB)
+print (numberA**numberB)
